20cb8fcb8a780049f5ec504c86fcceb7492655fc
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-05-04 23:37:23 +0600

    if loged in show book details and profile

--- src/Components/BooksCard.jsx/StoryBooks.jsx
+++ src/Components/BooksCard.jsx/StoryBooks.jsx
@@ -2,29 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 
 const StoryBooks = async () => {
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("http://localhost:3000/allbooks.json");
   const data = await res.json();
   console.log(data);
   return (
     <div className="container mx-auto md:py-12 py-5 ">
       
 
-      <div className="grid md:grid-cols-4 sm:grid-cols-2 gap-5 ">
+      <div className="grid md:grid-cols-4 sm:grid-cols-2 gap-5 justify-center items-center">
         {data.map((b) => (
           <div
-            key={b.id}
-            className="border items-center gap-3 rounded-2xl p-5 hover:shadow-lg transition-shadow duration-300 "
+            key={b.bookId}
+            className="items-center gap-3 rounded-2xl p-5 hover:shadow-lg transition-shadow duration-300 "
           >
-            <div className="flex">
-              <Image src={b.image_url} alt={b.id} width={200} height={200} />
+            
+              <Image src={b.image} alt={b.bookId} width={200} height={200} />
               <div>
-                <h3 className="text-xl pb-3">{b.bookName}</h3>
-                <p className="line-clamp-2">{b.title}</p>
+                <h3 className="text-xl pb-1">{b.bookName}</h3>
+                <p className="line-clamp-2">{b.author}</p>
               </div>
-            </div>
+            
 
-            <Link href={`/books/${b.id}`}>
-              <button className="btn btn-primary">Read</button>
+            <Link href={`/books/${b.bookId}`}>
+              <button className="btn btn-primary w-full">View Details</button>
             </Link>
           </div>
         ))}
--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -1,16 +1,30 @@
+import Image from "next/image";
+
 const page = async ({ params }) => {
   const { id } = await params;
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("http://localhost:3000/allbooks.json");
   const data = await res.json();
   console.log(data);
 
   console.log(id);
 
-  const book = data.find((b) => b.id == id);
+  const book = data.find((b) => b.bookId == id);
   console.log(book);
 
   return <div className="mt-20 container mx-auto">book details
-  <p>{book.title}</p>
+  <div>
+   <div className="mt-4 flex flex-col md:flex-row gap-5">  
+  <Image src={book.image} alt={book.bookName} width={300} height={400} />
+  <div className="w-100 h-">
+    <h2 className="text-4xl font-medium">{book.bookName}</h2>
+    <p className="text-lg font-medium">{book.author}</p>
+    <p className="mt-4 ">review: {book.review}</p>
+  </div>
+    
+  </div>
+  
+  </div>
+ 
   </div>;
 };
 
